978af73fe8b404d3004e0bf95b9ea664bc4a3a5a
zhangsheng <1070683532@qq.com>
2020-12-11 18:02:53 +0800

    添加配置

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,14 @@ let config = {
     port: "",
     baseUrl: "",
     type: env.VUE_APP_TITLE,
+    /**
+   * @description token key
+   */
+    TokenKey: 'EL-ADMIN-TOEKN',
+    /**
+   * @description 记住密码状态下的token在Cookie中存储的天数，默认1天
+   */
+    tokenCookieExpires: 1,
 }
 
 
