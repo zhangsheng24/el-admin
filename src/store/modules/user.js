c094906b4bbfbaa5ad0683275f10a580b5dcfb38
zhangsheng <1070683532@qq.com>
2020-12-14 18:12:14 +0800

    vuex保存登录信息，状态

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,23 +1,24 @@
 import {
-    getToken,
-    setToken,
-    removeToken
-} from '@/utils/auth'
+    CgetItem,
+    CsetItem,
+    CreItem
+} from '@/utils/storage'
 import api from '@/api'
+import config from '@/config'
 
 const user = {
     state: {
-        token:getToken(),
+        token: CgetItem('token'),
         user: {},
         roles: []
     },
 
     mutations: {
-        SET_TOKEN:(state,token)=>{
-            state.token=token
+        SET_TOKEN: (state, token) => {
+            state.token = token
         },
-        SET_ROLES:(state,roles)=>{
-            state.roles=roles
+        SET_ROLES: (state, roles) => {
+            state.roles = roles
         },
         SET_USER: (state, user) => {
             state.user = user
@@ -28,13 +29,16 @@ const user = {
     //对于模块内部的 action，局部状态通过 context.state 暴露出来,局部状态就是state
     actions: {
         //登录
-        Login({commit},userInfo){
+        Login({ commit }, userInfo) {
             const rememberMe = userInfo.rememberMe
-            return new Promise((resolve,reject)=>{
-                api('login.webLogin',userInfo).then(res=>{
-                    setToken(res.token, rememberMe)//token本cookie
-                    commit('SET_TOKEN', res.token)//登录成功之后给state里面的token设置一个最新的
-                    setUserInfo(res.user, commit)
+            return new Promise((resolve, reject) => {
+                api('login.webLogin', userInfo).then(res => {
+                    // 记住我就给个过期时间，不记住关闭浏览器失效
+                    rememberMe?CsetItem('token',res.token,{expires: config.passCookieExpires}):CsetItem('token',res.token)
+                    commit('SET_TOKEN', res.token)//登录成功之后给vuex中的模块user中的state里面的token设置一个最新的
+                    resolve()
+                }).catch(err=>{
+                    reject(err)
                 })
             })
         }
@@ -45,14 +49,14 @@ const user = {
 
 // }
 
-export const setUserInfo = (res, commit) => {
-    //如果没有任何权限，则赋予一个默认的权限，避免请求死循环
-    if(res.roles.length === 0){
-        commit('SET_ROLES', ['ROLE_SYSTEM_DEFAULT'])//设置默认权限
-    }else{
-        commit('SET_ROLES', res.roles)//设置当前用户权限
-    }
-    commit('SET_USER',res.user)//设置用户信息
-}
+// export const setUserInfo = (res, commit) => {
+//     //如果没有任何权限，则赋予一个默认的权限，避免请求死循环
+//     if (res.roles.length === 0) {
+//         commit('SET_ROLES', ['ROLE_SYSTEM_DEFAULT'])//设置默认权限
+//     } else {
+//         commit('SET_ROLES', res.roles)//设置当前用户权限
+//     }
+//     commit('SET_USER', res.user)//设置用户信息
+// }
 
 export default user
