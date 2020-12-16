074046a2309adf4d54ae18f04022c0b4f59c343b
zhangsheng <1070683532@qq.com>
2020-12-16 10:49:14 +0800

    tijioa

--- src/api/resource.js
+++ src/api/resource.js
@@ -1,16 +1,8 @@
 import Login from './modules/login.js'
-<<<<<<< HEAD
-import menu from './modules/system/menu'
-export default {
-    ...Login,
-    system:{
-        ...menu
-=======
 import menus from './modules/system/menu'
 export default {
     ...Login,
     system:{
         ...menus  
->>>>>>> 15d072f09009c92cb6952034f1a5b784b5f7f54e
     }
 }
