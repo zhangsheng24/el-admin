b403c8626dd86ffd65fa50dc328188d2539e12f3
zhangsheng <1070683532@qq.com>
2020-12-18 11:51:49 +0800

    折线图

---
+++ src/views/dashboard/LineChart.vue
@@ -0,0 +1,10 @@
+<template>
+  <div :class="className" :style="{height:height,width:width}" />
+</template>
+<script>
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
+export default {
+    
+}
+</script>
